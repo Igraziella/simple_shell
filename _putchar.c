2e9b28152203d36ee8e0b365069ccf8237deb1da
Igraziella <igraziella2@gmail.com>
2022-11-09 10:25:40 -0800

    putchar file for this project

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @prmChar: The character to print
+ *
+ * Return: number of printed chars.
+ */
+
+int _putchar(char prmChar)
+{
+	return (write(STDIN_FILENO, &prmChar, 1));
+}
